ddf94f4c57418fa0a8236c90c0eb270c4536d23f
artemskripnikov <artyom.skripnikov@bk.ru>
2025-01-30 19:10:00 +0300

    Deleted exclamation marks

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Привет, мир!");
-        System.out.println("Hello world!");
+        System.out.println("Привет, мир");
+        System.out.println("Hello world");
     }
 }
